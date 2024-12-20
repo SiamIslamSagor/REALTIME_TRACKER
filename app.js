4164bc6f9a17c7869c082332fafebce97619d216
SiamIslamSagor <msiamislam12345@gmail.com>
2024-12-20 15:38:05 +0600

    debug

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const socketio = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,4 +25,4 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(3000);
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
