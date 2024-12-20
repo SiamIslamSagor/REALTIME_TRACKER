abe7f455b947db12790bc0a84acb4ebccdd782b5
SiamIslamSagor <msiamislam12345@gmail.com>
2024-12-20 15:57:14 +0600

    debug3

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const io = socketio(server);
 const PORT = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
